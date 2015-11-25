09e0feb037e46c539167c009e4a13cc65d6c0682
3n4rKy <enarky@gmx.net>
2015-11-25 19:48:45 +0100

    first try for mjpeg receiver

--- piclienta/src/com/piclienta/main/MainActivity.java
+++ piclienta/src/com/piclienta/main/MainActivity.java
@@ -1,14 +1,25 @@
 package com.piclienta.main;
 
+import java.io.IOException;
 import java.net.InetAddress;
+import java.net.URI;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
 
 import com.piclienta.R;
 import com.piclienta.audio.Recorder;
 import com.piclienta.settings.GetIpWorker;
 import com.piclienta.settings.ReceiveIp;
 import com.piclienta.settings.Settings;
+import com.piclienta.video.MjpegInputStream;
+import com.piclienta.video.MjpegView;
 import com.util.PacketSender;
 
 import android.app.Activity;
@@ -16,9 +27,12 @@ import android.app.Dialog;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -60,16 +74,74 @@ public class MainActivity extends Activity implements OnCheckedChangeListener {
 	public Timer timer;
 	Thread receiveIp = null;
 	int progress = 1;
+	private static final boolean DEBUG=false;
+    private static final String TAG = "MJPEG";
 
+    private MjpegView mv = null;
+    String URL;
+    
+    // for settings (network and resolution)
+    private static final int REQUEST_SETTINGS = 0;
+    
+    private int width = 640;
+    private int height = 480;
+    
+    private int ip_ad1 = 192;
+    private int ip_ad2 = 168;
+    private int ip_ad3 = 1;
+    private int ip_ad4 = 214;
+    private int ip_port = 8080;
+    private String ip_command = "?action=stream";
+    
+    private boolean suspending = false;
+    
+	final Handler handler = new Handler();
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		//MJPEG
+		 SharedPreferences preferences = getSharedPreferences("SAVED_VALUES", MODE_PRIVATE);
+	        width = preferences.getInt("width", width);
+	        height = preferences.getInt("height", height);
+	        ip_ad1 = preferences.getInt("ip_ad1", ip_ad1);
+	        ip_ad2 = preferences.getInt("ip_ad2", ip_ad2);
+	        ip_ad3 = preferences.getInt("ip_ad3", ip_ad3);
+	        ip_ad4 = preferences.getInt("ip_ad4", ip_ad4);
+	        ip_port = preferences.getInt("ip_port", ip_port);
+	        ip_command = preferences.getString("ip_command", ip_command);
+	                
+	        StringBuilder sb = new StringBuilder();
+	        String s_http = "http://";
+	        String s_dot = ".";
+	        String s_colon = ":";
+	        String s_slash = "/";
+	        sb.append(s_http);
+	        sb.append(ip_ad1);
+	        sb.append(s_dot);
+	        sb.append(ip_ad2);
+	        sb.append(s_dot);
+	        sb.append(ip_ad3);
+	        sb.append(s_dot);
+	        sb.append(ip_ad4);
+	        sb.append(s_colon);
+	        sb.append(ip_port);
+	        sb.append(s_slash);
+	        sb.append(ip_command);
+	        URL = new String(sb);
+		
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 		setContentView(R.layout.activity_main);
-
+		
+		mv = (MjpegView) findViewById(R.id.MJPEGView);  
+        if(mv != null){
+        	mv.setResolution(width, height);
+        }
+		
+        new DoRead().execute(URL);
+        
 		btnForward = (ImageButton) findViewById(R.id.fwd);
 		btnForwardRight = (ImageButton) findViewById(R.id.fwdrgt);
 		btnForwardLeft = (ImageButton) findViewById(R.id.fwdlft);
@@ -232,6 +304,106 @@ public class MainActivity extends Activity implements OnCheckedChangeListener {
 			}
 		});
 	}
+	
+	public void onResume() {
+    	if(DEBUG) Log.d(TAG,"onResume()");
+        super.onResume();
+        if(mv!=null){
+        	if(suspending){
+        		new DoRead().execute(URL);
+        		suspending = false;
+        	}
+        }
+
+    }
+
+    public void onStart() {
+    	if(DEBUG) Log.d(TAG,"onStart()");
+        super.onStart();
+    }
+    
+    public void onPause() {
+    	if(DEBUG) Log.d(TAG,"onPause()");
+        super.onPause();
+        if(mv!=null){
+        	if(mv.isStreaming()){
+		        mv.stopPlayback();
+		        suspending = true;
+        	}
+        }
+    }
+    public void onStop() {
+    	if(DEBUG) Log.d(TAG,"onStop()");
+        super.onStop();
+    }
+
+    public void onDestroy() {
+    	if(DEBUG) Log.d(TAG,"onDestroy()");
+    	
+    	if(mv!=null){
+    		mv.freeCameraMemory();
+    	}
+    	
+        super.onDestroy();
+    }
+    
+       
+    public class DoRead extends AsyncTask<String, Void, MjpegInputStream> {
+        protected MjpegInputStream doInBackground(String... url) {
+            //TODO: if camera has authentication deal with it and don't just not work
+            HttpResponse res = null;         
+            DefaultHttpClient httpclient = new DefaultHttpClient(); 
+            HttpParams httpParams = httpclient.getParams();
+            HttpConnectionParams.setConnectionTimeout(httpParams, 5*1000);
+            HttpConnectionParams.setSoTimeout(httpParams, 5*1000);
+            if(DEBUG) Log.d(TAG, "1. Sending http request");
+            try {
+                res = httpclient.execute(new HttpGet(URI.create(url[0])));
+                if(DEBUG) Log.d(TAG, "2. Request finished, status = " + res.getStatusLine().getStatusCode());
+                if(res.getStatusLine().getStatusCode()==401){
+                    //You must turn off camera User Access Control before this will work
+                    return null;
+                }
+                return new MjpegInputStream(res.getEntity().getContent());  
+            } catch (ClientProtocolException e) {
+            	if(DEBUG){
+	                e.printStackTrace();
+	                Log.d(TAG, "Request failed-ClientProtocolException", e);
+            	}
+                //Error connecting to camera
+            } catch (IOException e) {
+            	if(DEBUG){
+	                e.printStackTrace();
+	                Log.d(TAG, "Request failed-IOException", e);
+            	}
+                //Error connecting to camera
+            }
+            return null;
+        }
+
+        protected void onPostExecute(MjpegInputStream result) {
+            mv.setSource(result);
+            if(result!=null){
+            	result.setSkip(1);
+            	setTitle(R.string.app_name);
+            }
+            mv.setDisplayMode(MjpegView.SIZE_BEST_FIT);
+            mv.showFps(false);
+        }
+    }
+    
+    public class RestartApp extends AsyncTask<Void, Void, Void> {
+        protected Void doInBackground(Void... v) {
+        	MainActivity.this.finish();
+            return null;
+        }
+
+        protected void onPostExecute(Void v) {
+        	startActivity((new Intent(MainActivity.this, MainActivity.class)));
+        }
+    }
+
+	
 
 	/**
 	 * A placeholder fragment containing a simple view.
